34de8ad9dce42557dc6c5e512f5a19c2de209b25
Giovanni <indirizzoemail@mail.com>
2021-09-01 18:19:20 +0200

    load database schema for new databases

--- src/database/api.rs
+++ src/database/api.rs
@@ -1,15 +1,31 @@
-use std::str::FromStr;
+use std::{path::Path, str::FromStr};
 
-use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
+use sqlx::{sqlite::SqliteConnectOptions, Executor, SqlitePool};
 
 pub type Database = SqlitePool;
 
+/// Connect to SQLite database
 pub async fn connect_or_create() -> Result<Database, sqlx::Error> {
-    let options = SqliteConnectOptions::from_str("sqlite:files.db")?
-        .journal_mode(sqlx::sqlite::SqliteJournalMode::Wal)
+    let database_path = "files.db";
+
+    let is_database_new = !Path::new(&database_path).exists();
+
+    let options = SqliteConnectOptions::from_str(&format!("sqlite:{}", &database_path))?
         .create_if_missing(true);
 
     let connection = SqlitePool::connect_with(options).await?;
 
+    if is_database_new {
+        load_schema(&connection).await;
+    }
+
     Ok(connection)
+}
+
+/// Load database schema
+pub async fn load_schema(database: &Database) {
+    database
+        .execute(include_str!("../schema.sql"))
+        .await
+        .expect("Cannot load database schema");
 }
