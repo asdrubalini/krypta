45dbf3a02306e0afa8e51603163c0baf95c7dd56
Giovanni <indirizzoemail@mail.com>
2021-09-04 02:10:04 +0200

    print archive total size

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,5 +1,14 @@
-use crate::database::Database;
+use byte_unit::Byte;
+
+use crate::database::{models, Database};
 
 pub async fn execute(database: &Database) {
-    todo!()
+    let archive_size_bytes = models::File::archive_size(database).await.unwrap();
+
+    let archive_size = Byte::from_bytes(archive_size_bytes.into());
+
+    println!(
+        "The total size of the archive is {}",
+        archive_size.get_appropriate_unit(false)
+    );
 }
