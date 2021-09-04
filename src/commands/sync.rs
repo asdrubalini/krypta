23fba066b4da5acf20237f03797c71ce2e11bf03
Giovanni <indirizzoemail@mail.com>
2021-09-04 14:34:36 +0200

    remove debug log

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -2,8 +2,6 @@ use crate::database::Database;
 use crate::sync::sync_database_from_source_folder;
 
 pub async fn execute(database: &Database, path: String) {
-    println!("here");
-
     let result = sync_database_from_source_folder(database, path)
         .await
         .expect("Fatal while executing command");
