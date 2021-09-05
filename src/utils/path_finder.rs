e63611b6b211b181308fcadf752579a8002c09a7
Giovanni <indirizzoemail@mail.com>
2021-09-05 15:38:13 +0200

    create some tests

--- src/utils/path_finder.rs
+++ src/utils/path_finder.rs
@@ -5,11 +5,11 @@ use walkdir::WalkDir;
 /// This trait is used in order to strip the "local bits" from a PathBuf
 /// so that it can be safely inserted into the database without polluting it
 /// with host-specific folders
-trait CanonicalizeAndSkipPathBuf<T> {
+trait CanonicalizeAndSkipPath<T> {
     fn canonicalize_and_skip_n(&mut self, n: usize) -> T;
 }
 
-impl CanonicalizeAndSkipPathBuf<PathBuf> for &Path {
+impl CanonicalizeAndSkipPath<PathBuf> for &Path {
     fn canonicalize_and_skip_n(&mut self, n: usize) -> PathBuf {
         self.canonicalize()
             .unwrap()
@@ -36,4 +36,32 @@ pub fn find_paths_relative(full_source_path: &Path) -> Vec<PathBuf> {
         })
         .map(|e| e.path().canonicalize_and_skip_n(full_source_path_length))
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::{create_dir_all, remove_dir_all, File};
+
+    use super::*;
+
+    #[test]
+    fn test_path_finder() {
+        let source_path = Path::new("/tmp/test_dir/foo/bar/");
+        create_dir_all(source_path).unwrap();
+
+        for i in 0..256 {
+            let mut filename = PathBuf::from(source_path);
+            filename.push(format!("file_{}", i));
+
+            File::create(filename).unwrap();
+        }
+
+        let relative_paths_found = find_paths_relative(source_path);
+
+        for path in relative_paths_found {
+            assert!(path.to_string_lossy().to_string().starts_with("file_"));
+        }
+
+        remove_dir_all(source_path).unwrap();
+    }
 }
