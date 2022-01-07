ae4fdd536a0786899c3da8773627a1d2f7314af5
Asdrubalini <indirizzoemail@mail.com>
2022-01-07 18:54:48 +0100

    database: port utils.rs to rusqlite

--- src/database/src/errors.rs
+++ src/database/src/errors.rs
@@ -1,9 +1,11 @@
 use thiserror::Error;
 
+pub type DatabaseResult<T> = Result<T, DatabaseError>;
+
 #[derive(Debug, Error)]
 pub enum DatabaseError {
-    #[error("SQLx Error: {0}")]
-    SQLx(#[from] sqlx::Error),
+    #[error("Sqlite error: {0}")]
+    SQLx(#[from] rusqlite::Error),
     #[error("Input/Output error")]
     IOError(#[from] std::io::Error),
 }
--- src/database/src/lib.rs
+++ src/database/src/lib.rs
@@ -1,9 +1,9 @@
-pub(crate) mod api;
-pub use api::{connect_or_create, create_in_memory, Database};
+mod utils;
+pub use utils::{connect_or_create, create_in_memory, Database};
 
-mod bigint_as_blob;
-pub use bigint_as_blob::BigIntAsBlob;
+// mod bigint_as_blob;
+// pub use bigint_as_blob::BigIntAsBlob;
 
 pub mod errors;
-pub mod models;
-pub mod traits;
+// pub mod models;
+// pub mod traits;
