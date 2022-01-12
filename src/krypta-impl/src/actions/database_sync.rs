415c4bcf7454d5fe30249cbeee323ec385471242
Asdrubalini <indirizzoemail@mail.com>
2022-01-12 20:10:38 +0100

    further prototype

--- src/krypta-impl/src/actions/database_sync.rs
+++ src/krypta-impl/src/actions/database_sync.rs
@@ -11,7 +11,7 @@ use database::{
     Database,
 };
 
-pub fn sync_database_from_unlocked_path(
+pub async fn sync_database_from_unlocked_path(
     db: &mut Database,
     unlocked_path: impl AsRef<Path>,
     device: &Device,
@@ -19,11 +19,21 @@ pub fn sync_database_from_unlocked_path(
     // Find paths that need to be inserted into the database
     let paths_requiring_insertion = find_paths_requiring_insertion(db, unlocked_path)?;
 
-    // TODO: run concurrently
-    // Find Metadata for paths that need to be inserted
-    let metadatas = find_metadata_for_paths(&paths_requiring_insertion)?;
-    // Compute hash for paths that need to be inserted
-    let hashes = find_hash_for_paths(&paths_requiring_insertion)?;
+    let (metadatas, hashes) = crossbeam::thread::scope(|s| {
+        // Find Metadata for paths that need to be inserted
+        let metadatas_handle = s.spawn(|_| find_metadata_for_paths(&paths_requiring_insertion));
+        // Compute hash for paths that need to be inserted
+        let hashes_handle = s.spawn(|_| find_hash_for_paths(&paths_requiring_insertion));
+
+        (
+            metadatas_handle.join().unwrap(),
+            hashes_handle.join().unwrap(),
+        )
+    })
+    .unwrap();
+
+    let metadatas = metadatas?;
+    let hashes = hashes?;
 
     // Obtain models::InsertFile and populate the database
     let insert_files = paths_requiring_insertion
@@ -38,7 +48,17 @@ pub fn sync_database_from_unlocked_path(
         .collect::<Vec<models::InsertFile>>();
 
     // Insert models::FileDevice
-    Ok(models::InsertFile::insert_many(db, &insert_files)?)
+    let inserted_files = models::InsertFile::insert_many(db, &insert_files)?;
+
+    let file_devices = inserted_files
+        .iter()
+        .map(|file| models::FileDevice::new(&file, device, true, false))
+        .collect::<Vec<_>>();
+
+    // Insert models::FileDevice
+    models::FileDevice::insert_many(db, &file_devices)?;
+
+    Ok(inserted_files)
 }
 
 /// Find all files in `unlocked_path` that need to be inserted into the database
