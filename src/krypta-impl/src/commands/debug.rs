370b93aa462cc5fd987f6d25c9a7e5de5492faba
Asdrubalini <indirizzoemail@mail.com>
2022-01-12 22:55:59 +0100

    use device config implementation

--- src/krypta-impl/src/commands/debug.rs
+++ src/krypta-impl/src/commands/debug.rs
@@ -1,14 +1,14 @@
-use std::path::PathBuf;
-
 use database::{models, Database};
 
 use crate::actions::database_sync::sync_database_from_unlocked_path;
 
 pub async fn execute(db: &mut Database) -> anyhow::Result<()> {
-    let unlocked_path = PathBuf::from("/vault/test_data/");
-    let _locked_path = PathBuf::from("/vault/encrypted/");
-
     let current_device = models::Device::find_or_create_current(db)?;
+
+    let unlocked_path = models::DeviceConfig::get_unlocked_path(db, &current_device)?
+        .expect("Cannot find unlocked_path.");
+    let _locked_path = models::DeviceConfig::get_locked_path(db, &current_device)?
+        .expect("Cannot find locked_path.");
 
     let inserted_files =
         sync_database_from_unlocked_path(db, unlocked_path, &current_device).await?;
