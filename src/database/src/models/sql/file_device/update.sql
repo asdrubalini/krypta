75a4e4cd8cb874fce1b17a5a35b3fee33320f449
unixLike <46504992+asdrubalini@users.noreply.github.com>
2022-01-13 11:11:28 +0100

    implement last_modified field in file_device (#44)

--- src/database/src/models/sql/file_device/update.sql
+++ src/database/src/models/sql/file_device/update.sql
@@ -1,6 +1,7 @@
 UPDATE `file_device`
 SET `is_unlocked` = ?,
-    `is_encrypted` = ?
+    `is_encrypted` = ?,
+    `last_modified` = ?
 WHERE 
     `file_id` = ? AND `device_id` = ?
-RETURNING `file_id`, `device_id`, `is_unlocked`, `is_encrypted`;
+RETURNING `file_id`, `device_id`, `is_unlocked`, `is_encrypted`, `last_modified`;
