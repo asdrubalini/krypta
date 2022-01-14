db76f713c0e007d793451f519bfb6b446a6e16da
Asdrubalini <indirizzoemail@mail.com>
2022-01-14 12:27:41 +0100

    rename stuff and document

--- src/database/src/traits.rs
+++ src/database/src/traits.rs
@@ -20,14 +20,17 @@ pub trait Fetch: Sized {
     fn fetch_all(db: &Database) -> DatabaseResult<Vec<Self>>;
 }
 
-pub trait UpdateMany: Sized {
-    fn update_many(db: &mut Database, updatables: &[Self]) -> DatabaseResult<Vec<Self>>;
-}
-
+/// A model that can be updated
 pub trait Update: Sized {
     fn update(&self, db: &Database) -> DatabaseResult<Self>;
 }
 
+/// A model that can be mass-updated
+pub trait UpdateMany: Sized {
+    fn update_many(db: &mut Database, updatables: &[Self]) -> DatabaseResult<Vec<Self>>;
+}
+
+/// A model that can be counted
 pub trait Count: Sized {
     fn count(db: &Database) -> DatabaseResult<i64>;
 }
