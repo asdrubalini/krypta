322c567bcad0faf726355ff2027e2e54fbc37334
Asdrubalini <indirizzoemail@mail.com>
2022-01-14 20:26:26 +0100

    fix debug

--- src/krypta-impl/src/commands/debug.rs
+++ src/krypta-impl/src/commands/debug.rs
@@ -5,8 +5,12 @@ use database::{models, Database};
 pub async fn execute(db: &mut Database) -> anyhow::Result<()> {
     let current_device = models::Device::find_or_create_current(db)?;
 
-    models::DeviceConfig::set_unlocked_path(db, PathBuf::from("/vault/locked/"), &current_device)?;
-    models::DeviceConfig::set_locked_path(db, PathBuf::from("/vault/unlocked/"), &current_device)?;
+    models::DeviceConfig::set_unlocked_path(
+        db,
+        PathBuf::from("/vault/unlocked/"),
+        &current_device,
+    )?;
+    models::DeviceConfig::set_locked_path(db, PathBuf::from("/vault/locked/"), &current_device)?;
 
     Ok(())
 }
