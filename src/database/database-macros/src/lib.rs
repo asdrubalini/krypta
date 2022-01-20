48d4132da57ddd61671498b1b529be2215be281d
Asdrubalini <indirizzoemail@mail.com>
2022-01-20 11:53:29 +0100

    use to string instead

--- src/database/database-macros/src/lib.rs
+++ src/database/database-macros/src/lib.rs
@@ -7,12 +7,29 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive_table_name(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
 
-    let table_name = format!("{ident}").to_case(Case::Snake);
+    let table_name = ident.to_string().to_case(Case::Snake);
 
     let output = quote! {
         impl TableName for #ident {
             fn table_name() -> &'static str {
                 #table_name
+            }
+        }
+    };
+    
+    output.into()
+}
+
+#[proc_macro_derive(TryFromRow)]
+pub fn derive_try_from_row(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, .. } = parse_macro_input!(input);
+
+    let output = quote! {
+        impl TryFromRow for #ident {
+            fn try_from_row(row: &Row) -> Result<Self, rusqlite::Error> {
+                Ok(#ident {
+                    id: row.get(0)?,
+                })
             }
         }
     };
