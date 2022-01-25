b74f3051e6353d436802800097a676f900fa331d
Asdrubalini <indirizzoemail@mail.com>
2022-01-25 10:20:03 +0100

    remove file_search

--- src/database/src/models/file_search.rs
+++
@@ -1,6 +0,0 @@
-use database_macros::TryFromRow;
-
-#[derive(TryFromRow)]
-pub struct FileSearch {
-    pub path: String,
-}
--- src/database/src/models/mod.rs
+++ src/database/src/models/mod.rs
@@ -2,12 +2,10 @@ mod device;
 mod device_config;
 mod file;
 mod file_device;
-mod file_search;
 mod tag;
 
 pub use device::Device;
 pub use device_config::DeviceConfig;
 pub use file::{File, MetadataFile};
 pub use file_device::{metadata_to_last_modified, FileDevice};
-pub use file_search::FileSearch;
 pub use tag::Tag;
