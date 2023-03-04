96a7ea5c758f170f018165ed21ea00e73ff7c2e7
Asdrubalini <asdrubalini@mail.com>
2023-03-05 00:17:17 +0100

    use once_cell for config

--- src/krypta-impl/src/utils/config.rs
+++ src/krypta-impl/src/utils/config.rs
@@ -2,17 +2,21 @@ use std::{
     fs::File,
     io::{Read, Write},
     path::PathBuf,
+    sync::RwLock,
 };
 
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Debug, Serialize, Deserialize)]
+static CONFIG: Lazy<RwLock<Config>> = Lazy::new(|| RwLock::from(Config::read_from_disk()));
+
+#[derive(Default, Debug, Serialize, Deserialize, Clone)]
 pub struct Config {
     pub locked_path: Option<String>,
 }
 
 impl Config {
-    pub(crate) fn get() -> Self {
+    fn read_from_disk() -> Self {
         let config_path = config_file_path();
 
         if config_path.exists() {
@@ -26,6 +30,11 @@ impl Config {
         }
     }
 
+    pub(crate) fn get() -> Self {
+        // read from global config
+        CONFIG.read().unwrap().to_owned()
+    }
+
     pub(crate) fn set(config: Self) {
         let config_path = config_file_path();
 
@@ -37,6 +46,10 @@ impl Config {
 
         let s = toml::to_string_pretty(&config).unwrap();
         f.write_all(s.as_bytes()).unwrap();
+
+        // Update global config
+        let mut c = CONFIG.write().unwrap();
+        *c = config;
     }
 
     pub fn get_locked_path() -> PathBuf {
